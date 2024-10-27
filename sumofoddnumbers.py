388a70b9bf0ebf87117ea4182d9fc366819096dd
jamilshehab <jamilshehab>
2024-10-27 12:20:18 +0200

    changes added

--- sumofoddnumbers.py
+++ sumofoddnumbers.py
@@ -1,3 +1,5 @@
+#trying my best to explain the code 
+
 #declare a variable called user number 
 user_number=int(input("enter the user number:"))
 #assign total to zero 
@@ -6,7 +8,7 @@ total=0
 for number in range(1,user_number):
     #if the number of remainder 2 is not equal to zero 
     if number % 2!=0:
-       #add the number to the total 
+       #calculate the total according to the number increment
        total+=number
 #print the total of the odd numbers
 print("the sum of the provided odd number is : " , total)
