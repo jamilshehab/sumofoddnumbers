e5a3e28945414f7278bfb0937c5672c5883265c2
jamilshehab <jamilshehab>
2024-10-27 11:36:35 +0200

    changes added

---
+++ readme.md
@@ -0,0 +1,2 @@
+Create a program that prompts the user to enter a number n
+and then calculates and displays the sum of all odd numbers up to that number n.
---
+++ sumofoddnumbers.py
@@ -0,0 +1,12 @@
+#declare a variable called user number 
+user_number=int(input("enter the user number:"))
+#assign total to zero 
+total=0
+#perform a loop through the user input
+for number in range(1,user_number):
+    #if the number of remainder 2 is not equal to zero 
+    if number % 2!=0:
+       #add the number to the total 
+       total+=number
+#print the total of the odd numbers
+print("the sum of the provided odd number is : " , total)
